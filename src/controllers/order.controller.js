b5efa55509669b3dbcb490e4ed29159e4b526a6b
Roaa Mounir <your-email@example.com>
2025-10-25 19:52:18 +0300

    Edit order

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -82,3 +82,18 @@ exports.updateOrderStatus = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const order = await orderModel.findByIdAndDelete(id);
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    res.status(200).json({ message: "Order deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
