d9f4d2f5f5a9d579b21d46547a92dbd1a4dd88f6
SandyAzzat <sandy.azzat.youssef@gmail.com>
2025-10-25 09:42:48 +0300

    Fix or update OAuth route logic

--- src/routes/oauth.route.js
+++ src/routes/oauth.route.js
@@ -3,10 +3,35 @@ const passport = require("passport");
 const router = express.Router();
 const { googleCallback, facebookCallback } = require("../controllers/oauth.controller");
 
-router.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-router.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/login", session: false }), googleCallback);
+router.get( "/auth/google",passport.authenticate("google", { scope: ["profile", "email"] }));
 
-router.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email"] }));
-router.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login", session: false }), facebookCallback);
+router.get("/auth/google/callback", (req, res, next) => {
+    if (req.query.error) {return res.redirect("http://localhost:4200/login?error=cancelled");}
+
+    passport.authenticate("google", { failureRedirect: "/login", session: false }, (err, user) => {
+        if (err || !user) {
+            return res.redirect("http://localhost:4200/login?error=failed");
+        }
+        req.user = user;
+        googleCallback(req, res);
+    })(req, res, next);
+});
+
+
+router.get("/auth/facebook",passport.authenticate("facebook", { scope: ["email"] }));
+
+router.get("/auth/facebook/callback", (req, res, next) => {
+    if (req.query.error) {
+        return res.redirect("http://localhost:4200/login?error=cancelled");
+    }
+
+    passport.authenticate("facebook", { failureRedirect: "/login", session: false }, (err, user) => {
+        if (err || !user) {
+        return res.redirect("http://localhost:4200/login?error=failed");
+        }
+        req.user = user;
+        facebookCallback(req, res);
+    })(req, res, next);
+});
 
 module.exports = router;
