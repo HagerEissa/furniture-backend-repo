f89cebe2b45acc7a74ebde3af83baf94c758c140
Hager Eissa <hagersalah050@gmail.com>
2025-10-28 04:38:03 +0300

    add midd on routes

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cart.controller')
 
+const ROLES = require("../utils/roles.util");
+const roleMiddleware = require("../middlewares/role.middleware");
+const authMiddleware = require("../middlewares/auth.middleware"); 
 
-router.get('/:userId',cartController.getCartForUser);
-router.post('/add',cartController.addToCart);
-router.delete('/:userId/:productId',cartController.deleteProductFromCart);
-router.put('/:userId/:productId',cartController.updateQuantity);
-router.delete('/clear/:userId', cartController.clearCart);
+
+router.get('/:userId',authMiddleware,roleMiddleware(ROLES.USER),cartController.getCartForUser);
+router.post('/add',authMiddleware,roleMiddleware(ROLES.USER),cartController.addToCart);
+router.delete('/:userId/:productId',authMiddleware,roleMiddleware(ROLES.USER),cartController.deleteProductFromCart);
+router.put('/:userId/:productId',authMiddleware,roleMiddleware(ROLES.USER),cartController.updateQuantity);
+router.delete('/clear/:userId', authMiddleware,roleMiddleware(ROLES.USER),cartController.clearCart);
 
 module.exports = router;
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -4,13 +4,18 @@ const productController = require('../controllers/product.controller')
 
 const upload = require('../config/multerConfig');
 
-router.get('/list', productController.getProductForList);
-router.get('/new', productController.getNewProducts);
-router.get('/:id',productController.getProductById);
-router.get('/',productController.getAllProducts);
-router.post('/',upload.single('productImage'),productController.addProduct);
-router.put('/:id',upload.single('productImage'),productController.updateProduct);
-router.delete('/:id',productController.deleteProduct);
+const ROLES = require("../utils/roles.util");
+const roleMiddleware = require("../middlewares/role.middleware");
+const authMiddleware = require("../middlewares/auth.middleware"); 
+
+
+router.get('/list',authMiddleware ,productController.getProductForList);
+router.get('/new', authMiddleware,productController.getNewProducts);
+router.get('/:id',authMiddleware,productController.getProductById);
+router.get('/',authMiddleware,productController.getAllProducts);
+router.post('/',authMiddleware,roleMiddleware(ROLES.ADMIN),upload.single('productImage'),productController.addProduct);
+router.put('/:id',authMiddleware,roleMiddleware(ROLES.ADMIN),upload.single('productImage'),productController.updateProduct);
+router.delete('/:id',authMiddleware,roleMiddleware(ROLES.ADMIN),productController.deleteProduct);
 
 
 module.exports = router;
