c2adb5e7b059150239ca31c7eed23e3169de3a0d
Roaa Mounir <your-email@example.com>
2025-10-30 16:53:32 +0300

    editOrder

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.post('/', authMiddleware, orderController.createOrder);
+router.get('/', authMiddleware, orderController.getAllOrders);
 router.get('/user', authMiddleware, orderController.getUserOrders);
 router.get('/:id', authMiddleware, orderController.getOrderById);
 router.delete('/:id', authMiddleware, orderController.deleteOrder);
@@ -13,3 +14,4 @@ router.delete('/:id', authMiddleware, orderController.deleteOrder);
 router.patch('/:id/status', authMiddleware, orderController.updateOrderStatus);
 
 module.exports = router;
+    
