77dfbe64fc525d3816789dbf6e9839212b3e193b
Roaa Mounir <your-email@example.com>
2025-11-10 18:56:50 +0200

    edit

--- src/app.js
+++ src/app.js
@@ -6,43 +6,49 @@ require("./config/passport.config");
 const path = require("path");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const FRONTEND_URL = "https://furniture-backend-repo-production.up.railway.app";
 
 const authRoutes = require("./routes/auth.route");
 const oauthRoutes = require("./routes/oauth.route");
 const reviewRoutes = require("./routes/review.route");
 const userRoutes = require("./routes/user.route");
-const contactRoutes = require("./routes/contact.route")
-const categoryRouter = require('./routes/category.router');
-const productRouter = require('./routes/product.router');
-const cartRouter = require('./routes/cart.router')
-const favouriteRouter = require('./routes/favourite.router')
-const orderRouter = require('./routes/order.router');
+const contactRoutes = require("./routes/contact.route");
+const categoryRouter = require("./routes/category.router");
+const productRouter = require("./routes/product.router");
+const cartRouter = require("./routes/cart.router");
+const favouriteRouter = require("./routes/favourite.router");
+const orderRouter = require("./routes/order.router");
 const paymentRoutes = require("./routes/payment.route");
 const webhookRoutes = require("./routes/webhook.route");
 
 const app = express();
 app.use(
-    helmet({
-        contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            imgSrc: ["'self'","data:","blob:","res.cloudinary.com",],
-            scriptSrc: ["'self'","'unsafe-inline'",],
-            styleSrc: ["'self'","'unsafe-inline'",],
-            connectSrc: ["'self'", "http://localhost:4200",],
-        },
-        },
-    })
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'", "data:", "blob:", "res.cloudinary.com"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        connectSrc: ["'self'", "http://localhost:4200", FRONTEND_URL],
+      },
+    },
+  })
 );
 
 app.use(
-    cors({
-        origin: "http://localhost:4200",
-        credentials: true,
-    })
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
 );
 
-app.use("/api/webhook", express.raw({ type: "application/json" }), webhookRoutes);
+app.use(
+  "/api/webhook",
+  express.raw({ type: "application/json" }),
+  webhookRoutes
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -51,11 +57,11 @@ app.use(passport.initialize());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: "Too many requests from this IP, please try again later",
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later",
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 app.use(apiLimiter);
@@ -64,19 +70,18 @@ app.use("/api", authRoutes);
 app.use("/api", oauthRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/product", reviewRoutes);
-app.use("/api/contact",contactRoutes)
+app.use("/api/contact", contactRoutes);
 
 app.use("/api/payment", paymentRoutes);
 
-app.use('/api/product',productRouter);
-app.use('/api/category',categoryRouter);
-app.use('/api/cart',cartRouter);
-app.use('/api/favourite',favouriteRouter);
-app.use('/api/orders', orderRouter);
+app.use("/api/product", productRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/favourite", favouriteRouter);
+app.use("/api/orders", orderRouter);
 
-
-app.use((req,res)=>{
-    res.status(404).json({ message: "Route not found" });
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 module.exports = app;
